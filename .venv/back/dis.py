31e9e2a7b0603fbb177dfacb8f0c7288633f0ce6
vital <vitalik.mishl.kotov@mail.ru>
2025-10-24 16:43:07 +0300

    Изменение в логике маршрутов

--- .venv/back/dis.py
+++ .venv/back/dis.py
@@ -30,3 +30,65 @@ def get_users():
         mimetype='application/json'
     )
     return response
+
+@dis_bp.route('/companies', methods=['GET'])
+def search_companies():
+    search_name = request.args.get('name')
+    search_ogrn = request.args.get('ogrn')
+
+    rez = []
+
+
+    if search_name and search_ogrn:
+        names = search_name.split('_')
+        ogrns = search_ogrn.split('_')
+
+        for name in names:
+            for ogrn in ogrns:
+                cur.execute("""
+                    SELECT * FROM public.Company 
+                    WHERE LOWER(name) LIKE LOWER(%s) 
+                    AND OGRN::TEXT LIKE %s;
+                """, (f'%{name}%', f'%{ogrn}%'))
+                rows = cur.fetchall()
+                for row in rows:
+                    rez.append({
+                        'ogrn': row[0],
+                        'name': row[1]
+                    })
+
+    elif search_name is not None:
+        names = search_name.split('_')
+        for name in names:
+            cur.execute("SELECT * FROM public.Company WHERE LOWER(name) LIKE LOWER(%s);",
+                        (f'%{name}%',))
+            rows = cur.fetchall()
+            for row in rows:
+                rez.append({
+                    'ogrn': row[0],
+                    'name': row[1]
+                })
+
+    elif search_ogrn is not None:
+        ogrns = search_ogrn.split('_')
+        for ogrn in ogrns:
+            cur.execute("SELECT * FROM public.Company WHERE OGRN::TEXT LIKE %s;",
+                        (f'%{ogrn}%',))
+            rows = cur.fetchall()
+            for row in rows:
+                rez.append({
+                    'ogrn': row[0],
+                    'name': row[1]
+                })
+
+    else:
+        cur.execute("SELECT * FROM public.Company ORDER BY name;")
+        rows = cur.fetchall()
+        for row in rows:
+            rez.append({
+                'ogrn': row[0],
+                'name': row[1]
+            })
+
+    rez = [dict(t) for t in {frozenset(d.items()) for d in rez}]
+    return jsonify(rez)
